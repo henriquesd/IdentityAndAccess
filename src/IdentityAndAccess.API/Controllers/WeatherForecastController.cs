bc1fd161124bc83987fd9cc250d8d297f3ae4465
henriquesd <hsd.foz@gmail.com>
2021-11-22 20:53:14 +0100

    Updated WeatherForecastController

--- src/IdentityAndAccess.API/Controllers/WeatherForecastController.cs
+++ src/IdentityAndAccess.API/Controllers/WeatherForecastController.cs
@@ -1,10 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityAndAccess.API.Controllers
 {
+    [Authorize]
     [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    [Route("api/[controller]")]
+    public class WeatherForecastController : MainController
     {
         private static readonly string[] Summaries = new[]
         {
@@ -18,8 +20,21 @@ namespace IdentityAndAccess.API.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet]
         public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("GetAnonymous")]
+        public IEnumerable<WeatherForecast> GetAnonymous()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
